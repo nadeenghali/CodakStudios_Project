3eaca146584e16db35233564a31ae4549da59cdb
Mariam-Sameh <mariamsameh96@gmail.com>
2018-12-09 16:08:15 +0200

    ui design

---
+++ Assets/UI/Scripts/GameOverScreenScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScreenScript : MonoBehaviour {
+
+    public Canvas gameOverCanvas;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            gameOverCanvas.GetComponent<Canvas>().enabled = true;
+            this.GetComponent<Canvas>().enabled = false;
+        }
+	}
+}
