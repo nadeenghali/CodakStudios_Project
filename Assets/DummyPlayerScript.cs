2c0c1fae2a868f946961a1702df63c52a75b1f40
Omar Alshazly <3omarAlshazly@gmail.com>
2018-12-10 23:35:35 +0200

    map design and scriptable doors

---
+++ Assets/DummyPlayerScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayerScript : MonoBehaviour {
+
+    float step = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Keyboard();
+	}
+
+
+    void Keyboard()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            step = 0.1f;
+            transform.Translate(new Vector3(0.0f, 0.0f, step));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            step = 0.1f;
+            transform.Translate(new Vector3(step, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            step = -0.1f;
+            transform.Translate(new Vector3(0.0f, 0.0f, step));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            step = -0.1f;
+            transform.Translate(new Vector3(step, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            step = -0.1f;
+            transform.Translate(new Vector3(0.0f, step, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            step = 0.1f;
+            transform.Translate(new Vector3(0.0f, step, 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0.0f, -5.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0.0f, 5.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            //transform.Rotate(5.0f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            //transform.Rotate(-5.0f, 0.0f, 0.0f);
+        }
+
+    }
+}
