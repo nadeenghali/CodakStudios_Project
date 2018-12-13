a4f0b1e31b06e38e7896a702b86605fbf430c991
saranagui <sara.antoun@student.guc.edu.eg>
2018-12-13 21:28:34 +0200

    added health chests in map

---
+++ Assets/Sounds and Music/Sounds/Effects/Scripts/HandleEffectsVolume.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleEffectsVolume : MonoBehaviour {
+    public float volume;
+    public GameObject[] effectsSources;
+
+    // Use this for initialization
+    void Start()
+    {
+        volume = AudioCanvas.music;
+
+        effectsSources = GameObject.FindGameObjectsWithTag("Effect");
+        foreach (GameObject effectsSource in effectsSources)
+        {
+            effectsSource.GetComponent<AudioSource>().volume = volume;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        volume = AudioCanvas.music;
+        foreach (GameObject speechSource in effectsSources)
+        {
+            speechSource.GetComponent<AudioSource>().volume = volume;
+        }
+    }
+}
